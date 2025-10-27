9078fc01d10f69a97d3a7341f1e6af363ad84276
Jesús Alberto Revilla Silva <jesus_revilla@yahoo.com>
2025-10-27 16:12:34 -0600

    Create customers and orders tables
    Added SQL commands to create customers and orders tables.

--- 01_create_tables.sql
+++ 01_create_tables.sql
@@ -1,1 +1,14 @@
 
+-- Crear tablas
+CREATE TABLE customers (
+    id INT PRIMARY KEY,
+    name VARCHAR(50),
+    country VARCHAR(50)
+);
+
+CREATE TABLE orders (
+    id INT PRIMARY KEY,
+    customer_id INT,
+    product VARCHAR(50)
+);
+
