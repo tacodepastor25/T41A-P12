8c521fa10b8fc77d30935c88d37547d45d4c2d8f
tacodepastor25 <125671857+tacodepastor25@users.noreply.github.com>
2025-10-27 22:43:15 -0600

    Update 06_full_outer_join.sql

--- 06_full_outer_join.sql
+++ 06_full_outer_join.sql
@@ -1,2 +1,2 @@
 -- FULL OUTER JOIN: todos los clientes y pedido
-SELECT * FROM customers;
+SELECT customers.name, orders.product FROM customers FULL OUTER JOIN orders ON orders.customer_id = customers.id;
