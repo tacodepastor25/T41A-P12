1307b5d9e2b893776aea4e3c7f5f89b9530ea907
tacodepastor25 <125671857+tacodepastor25@users.noreply.github.com>
2025-10-27 22:42:04 -0600

    Update 05_right_join.sql

--- 05_right_join.sql
+++ 05_right_join.sql
@@ -1,2 +1,2 @@
 -- RIGHT JOIN: todos los pedidos, aunque no tengan cliente
-SELECT * FROM orders;
+SELECT orders.customer_id, orders.product, customers.* FROM customers RIGHT JOIN orders ON orders.customer_id = customers.id;
